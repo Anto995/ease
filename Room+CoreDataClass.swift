3d597650cb0765942ca612578ca5f5c9d571e4be
Antonio Scialdone <antonio@10-24-48-12.0200.sal.it.iosda.org>
2017-02-28 15:20:20 +0100

    CoreData

---
+++ Room+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Room+CoreDataClass.swift
+//  Ease
+//
+//  Created by Antonio Scialdone on 28/02/2017.
+//  Copyright © 2017 Antonio Scialdone. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Room)
+public class Room: NSManagedObject {
+
+}
