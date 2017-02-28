4fff43de0a08dbe3a87357e90cd2d623c3771439
Antonio Scialdone <antonio@10-24-48-12.0200.sal.it.iosda.org>
2017-02-28 09:22:38 +0100

    2

--- Ease/ViewController.swift
+++ Ease/ViewController.swift
@@ -10,8 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+     func firstTime() {
+        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
+        if launchedBefore  {
+            
+            print("Not first launch.")
+            performSegue(withIdentifier: "6", sender: nil)
+        } else {
+            
+            print("First launch, setting UserDefault.")
+            UserDefaults.standard.set(true, forKey: "launchedBefore")
+            performSegue(withIdentifier: "0", sender: nil)
+        }
+        
+    }
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +39,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        firstTime()
+    }
 
+    
+    
+    
 }
 
