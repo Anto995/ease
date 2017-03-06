f7faf2cc964786ba6dbdbc19072fbe650260cbb4
Antonio Scialdone <antonio@10-24-48-12.0200.sal.it.iosda.org>
2017-03-06 10:15:00 +0100

    Initial Commit

---
+++ sofApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sofApp
+//
+//  Created by Antonio Scialdone on 06/03/2017.
+//  Copyright © 2017 Antonio Scialdone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ sofAppTests/sofAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  sofAppTests.swift
+//  sofAppTests
+//
+//  Created by Antonio Scialdone on 06/03/2017.
+//  Copyright © 2017 Antonio Scialdone. All rights reserved.
+//
+
+import XCTest
+@testable import sofApp
+
+class sofAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
